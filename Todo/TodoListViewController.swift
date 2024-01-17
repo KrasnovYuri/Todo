1ee0158c71060da376270b01646ad2fbf52acf9a
KrasnovYuri <krasnov1yuri@gmail.com>
2024-01-17 21:39:30 +0300

    add UITableViewDataSource methods

---
+++ Todo/TodoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Юрий on 17.01.2024.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+
+}
+
